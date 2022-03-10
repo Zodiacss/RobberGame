1354762386448caeb2210b87cdfc4d3d43f81f71
Zodiacss <88719436+Zodiacss@users.noreply.github.com>
2022-03-10 17:12:00 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var background, bomb, police, puddle, robber
+
+function preload() {
+  loadImage("background.png");
+  loadImage("police.png");
+  loadImage("Bomb.jfif");
+  loadImage("Puddle.jfif");
+  loadImage("You lost.jpg");
+  loadImage("You win.jpg");
+}
+
+
+
+
+
